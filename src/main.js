8a621e54cf082c86d1fd42eab3f393103f9a38e0
Horinchoi Yan <maagikus@gmail.com>
2023-08-17 13:42:13 +0300

    add pinia store import and usage

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
+
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+import "./assets/styles/style.scss";
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.mount('#app')
